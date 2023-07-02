4f770a6038c463120f32826c823678a92de4fb60
lehung2022 <hung.le.programmer@gmail.com>
2023-07-02 22:50:16 +0700

    I fixed the Login file, and added api

--- login-chapter2/src/components/Login.jsx
+++ login-chapter2/src/components/Login.jsx
@@ -1,13 +1,19 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "../context/AuthContext";
+import axios from '../api/axios';
+
+
+const LOGIN_URL = '/auth';
 
 const Login = () => {
+    const { setAuth } = useContext(AuthContext);
     const userRef = useRef();
     const errRef = useRef();
 
     const [user, setUser] = useState('');
     const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState('');
+    const [success, setSuccess] = useState(false);
 
     useEffect(() => {
         userRef.current.focus();
